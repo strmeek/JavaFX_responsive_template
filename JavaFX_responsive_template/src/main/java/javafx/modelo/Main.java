d4543e6386a1fdbbcd407dc63f8dc541c9dfb173
Filipe Andrade <101667610+strmeek@users.noreply.github.com>
2023-01-21 11:26:52 -0300

    first commit, a little unorganized

---
+++ JavaFX_responsive_template/src/main/java/javafx/modelo/Main.java
@@ -0,0 +1,28 @@
+package javafx.modelo;
+
+import com.guigarage.responsive.ResponsiveHandler;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        /* Calls the FXML file and the css file to create the interactive interface(frontend) */
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml")); //gets the fxml file.
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720); // create the scene with the fxml info.
+        String css = this.getClass().getResource("style.css").toExternalForm(); // get the css file.
+        scene.getStylesheets().add(css); // add the css file to the scene.
+        stage.setTitle("Task Manager"); // title.
+        stage.setScene(scene); // display the scene.
+        stage.show(); // make visible.
+        ResponsiveHandler.addResponsiveToWindow(scene.getWindow());// add the responsivefx library to the scene
+    }
+
+    public static void main(String[] args) {
+        launch(); //launch the application.
+    }
+}
---
+++ JavaFX_responsive_template/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module javafx.modelo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+    requires responsivefx;
+    requires mysql.connector.j;
+
+    opens javafx.modelo to javafx.fxml;
+    exports javafx.modelo;
+}
